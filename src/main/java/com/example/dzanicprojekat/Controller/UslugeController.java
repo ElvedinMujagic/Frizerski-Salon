c1b94f6c47ce59cb7d6db7ab22f0153a566569d2
Elvedin <elvedinedo19@gmail.com>
2025-08-12 23:26:54 +0200

    Kontroler za Usluge

---
+++ src/main/java/com/example/dzanicprojekat/Controller/UslugeController.java
@@ -0,0 +1,52 @@
+package com.example.dzanicprojekat.Controller;
+
+import com.example.dzanicprojekat.Services.UslugeService;
+import com.example.dzanicprojekat.Utility.DTOs.UslugaDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class UslugeController {
+    private final UslugeService uslugeService;
+
+    public UslugeController(UslugeService uslugeService) {
+        this.uslugeService = uslugeService;
+    }
+
+    @GetMapping("/admin/sve_usluge")
+    public String sveUsluge(Model model) {
+        model.addAttribute("title","Sve Usluge");
+        model.addAttribute("usluge",uslugeService.getAllUsluga());
+        return "/dashboard/admin/sve_usluge";
+    }
+
+    @GetMapping("/admin/dodaj_usluge")
+    public String dodajUsluge(Model model) {
+        model.addAttribute("title","Dodaj Usluge");
+        model.addAttribute("uslugaDTO", new UslugaDTO());
+        return "/dashboard/admin/dodaj_usluge";
+    }
+
+    @PostMapping("/dodaj_uslugu")
+    public String dodajUslugu(@Valid @ModelAttribute UslugaDTO uslugaDTO, BindingResult bindingResult,Model model) {
+        System.out.println("Dodaj uslugu - End point hit");
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("title","Dodaj Usluge");
+            return "/dashboard/admin/dodaj_usluge";
+        }
+        uslugeService.addUsluga(uslugaDTO);
+        return "/dashboard/admin/dodaj_usluge";
+    }
+}
