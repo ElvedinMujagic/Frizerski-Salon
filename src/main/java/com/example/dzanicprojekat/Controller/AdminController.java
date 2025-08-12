098f759afd3e3a9a67fd4fcff1f92751f45c32bd
Elvedin <elvedinedo19@gmail.com>
2025-08-12 23:15:03 +0200

    -

--- src/main/java/com/example/dzanicprojekat/Controller/AdminController.java
+++ src/main/java/com/example/dzanicprojekat/Controller/AdminController.java
@@ -3,14 +3,9 @@ package com.example.dzanicprojekat.Controller;
 import com.example.dzanicprojekat.Services.AdminService;
 import com.example.dzanicprojekat.Services.FrizerService;
 import com.example.dzanicprojekat.Services.UserService;
-import com.example.dzanicprojekat.Utility.DTOs.UslugaDTO;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AdminController {
