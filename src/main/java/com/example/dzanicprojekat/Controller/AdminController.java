3985cc3163c8fd56ec3d71b4cfeb13e2d31e2dac
Elvedin <elvedinedo19@gmail.com>
2025-08-12 23:13:27 +0200

    Changes to file name and values

---
+++ src/main/java/com/example/dzanicprojekat/Controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.example.dzanicprojekat.Controller;
+
+import com.example.dzanicprojekat.Services.AdminService;
+import com.example.dzanicprojekat.Services.FrizerService;
+import com.example.dzanicprojekat.Services.UserService;
+import com.example.dzanicprojekat.Utility.DTOs.UslugaDTO;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AdminController {
+    private final UserService userService;
+    private final AdminService adminService;
+    private final FrizerService frizerService;
+
+    public AdminController(UserService userService, AdminService adminService, FrizerService frizerService) {
+        this.userService = userService;
+        this.adminService = adminService;
+        this.frizerService = frizerService;
+    }
+
+    @GetMapping("/admin/dodaj_frizera")
+    public String dodajFrizera(Model model) {
+        model.addAttribute("title","Dodaj Frizera");
+        return "/dashboard/admin/dodaj_frizera";
+    }
+}
