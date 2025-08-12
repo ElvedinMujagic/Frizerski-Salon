62441518a624d8f64d31e23eaeac83b421d54480
Elvedin <elvedinedo19@gmail.com>
2025-08-12 23:26:33 +0200

    Data-Transfer-Object Usluga

---
+++ src/main/java/com/example/dzanicprojekat/Utility/DTOs/UslugaDTO.java
@@ -0,0 +1,26 @@
+package com.example.dzanicprojekat.Utility.DTOs;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class UslugaDTO {
+    @NotBlank(message = "Naziv je potreban!!")
+    private String naziv;
+    @NotNull(message = "Cijena je potrebna!")
+    @Min(value = 1, message = "Cijena mora biti najmanje 1 KM")
+    private double cijena;
+    @NotNull(message = "Trajanje je potrebno!")
+    @Min(value = 1, message = "Trajanje mora biti najmanje 1 minuta")
+    private int trajanje;
+    private boolean active;
+
+}
