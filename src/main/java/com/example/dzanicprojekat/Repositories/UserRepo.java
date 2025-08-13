08c4fb2764acc8fa4a9c616de8d331b808c65c76
Elvedin <elvedinedo19@gmail.com>
2025-08-13 22:46:24 +0200

    Dodan findByRoleNotIn(List<Role> list)

--- src/main/java/com/example/dzanicprojekat/Repositories/UserRepo.java
+++ src/main/java/com/example/dzanicprojekat/Repositories/UserRepo.java
@@ -1,13 +1,17 @@
 package com.example.dzanicprojekat.Repositories;
 
 import com.example.dzanicprojekat.Entities.User;
+import com.example.dzanicprojekat.Utility.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepo extends JpaRepository<User,Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
     User readByUsername(String username);
-
+    User readById(Long id);
+    List<User> findByRoleNotIn(List<Role> roles);
 }
