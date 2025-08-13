54f51133377c7fe5fac0eb55cc3a6884e85038d0
Elvedin <elvedinedo19@gmail.com>
2025-08-13 22:48:23 +0200

    Omogućeno: Brisanje usluga Još ima raditi na validaciji

--- src/main/java/com/example/dzanicprojekat/Controller/UslugeController.java
+++ src/main/java/com/example/dzanicprojekat/Controller/UslugeController.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,12 +38,21 @@ public class UslugeController {
     @PostMapping("/dodaj_uslugu")
     public String dodajUslugu(@Valid @ModelAttribute UslugaDTO uslugaDTO, BindingResult bindingResult,Model model) {
         System.out.println("Dodaj uslugu - End point hit");
-
+        // TODO: Onemoguciti dodavanje ako usluga vec postoji (Ako imaju isti naziv)
         if (bindingResult.hasErrors()) {
             model.addAttribute("title","Dodaj Usluge");
             return "/dashboard/admin/dodaj_usluge";
         }
         uslugeService.addUsluga(uslugaDTO);
-        return "/dashboard/admin/dodaj_usluge";
+        return "redirect:/admin/dodaj_usluge";
+    }
+
+    @PostMapping("/obrisi_uslugu")
+    public String obrisiUslugu(@Valid @RequestParam Long id) {
+        System.out.println("Delete endpoint hit");
+        if (uslugeService.checkByID(id)) {
+            uslugeService.deleteUsluga(id);
+        }
+        return "redirect:/admin/sve_usluge";
     }
 }
