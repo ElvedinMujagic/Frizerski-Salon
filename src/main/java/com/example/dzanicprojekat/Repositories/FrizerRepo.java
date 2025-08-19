3b9e911d386211dbceb71251f43f1147ddbc783b
Elvedin <elvedinedo19@gmail.com>
2025-08-19 16:50:53 +0200

    Dodano: metoda za uzimanje svih frizera na osnovu da li su aktivni

--- src/main/java/com/example/dzanicprojekat/Repositories/FrizerRepo.java
+++ src/main/java/com/example/dzanicprojekat/Repositories/FrizerRepo.java
@@ -5,8 +5,12 @@ import com.example.dzanicprojekat.Entities.Frizer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FrizerRepo extends JpaRepository<Frizer,Long> {
     Frizer readById(long id);
+    void deleteById(long id);
+    List<Frizer> findAllByAvailable(boolean isActive);
 
 }
