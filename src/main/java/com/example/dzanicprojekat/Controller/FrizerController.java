6df23a2de087325425279b64129d8f82c50648fa
Elvedin <elvedinedo19@gmail.com>
2025-08-19 16:50:04 +0200

    Dodana funkcionalnost vidjenja svih frizera i njihovo brisanje/dodavanje

--- src/main/java/com/example/dzanicprojekat/Controller/FrizerController.java
+++ src/main/java/com/example/dzanicprojekat/Controller/FrizerController.java
@@ -1,5 +1,6 @@
 package com.example.dzanicprojekat.Controller;
 
+import com.example.dzanicprojekat.Entities.User;
 import com.example.dzanicprojekat.Services.AdminService;
 import com.example.dzanicprojekat.Services.FrizerService;
 import com.example.dzanicprojekat.Services.UserService;
@@ -11,6 +12,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class FrizerController {
@@ -24,15 +28,21 @@ public class FrizerController {
         this.frizerService = frizerService;
     }
 
-    @GetMapping("/admin/dodaj_frizera_form")
+    @GetMapping("/admin/svi_frizeri_page")
+    public String getFrizers(Model model) {
+        model.addAttribute("user_data",userService.getFrizersOnly());
+        return "/dashboard/admin/svi_frizeri";
+    }
+
+    @GetMapping("/admin/dodaj_frizera_page")
     public String getForm(Model model) {
         model.addAttribute("title","Dodaj Frizera");
-        model.addAttribute("users", userService.getUsersOnly());
+        model.addAttribute("users", userService.getClientsOnly());
         model.addAttribute("frizerDTO", new FrizerDTO());
         return "/dashboard/admin/dodaj_frizera";
     }
 
-    @PostMapping("/dodaj_frizera")
+    @PostMapping("/dodaj_frizera_request")
     public String dodajFrizera(@Valid @ModelAttribute FrizerDTO frizerDTO, BindingResult result,Model model) {
         if (result.hasErrors()) {
             model.addAttribute("title","Dodaj Frizera");
@@ -44,6 +54,12 @@ public class FrizerController {
         *   3.) Ako korisnik unese negativan broj */
         System.out.println("TEST");
         frizerService.addFrizer(frizerDTO);
-        return "redirect:/admin/dodaj_frizera_form";
+        return "redirect:/admin/dodaj_frizera_page";
+    }
+
+    @PostMapping("/obrisi_frizera_request")
+    public String obrisiFrizera(@Valid @RequestParam Long id) {
+        frizerService.deleteFrizer(id);
+        return "redirect:/admin/svi_frizeri_page";
     }
 }
