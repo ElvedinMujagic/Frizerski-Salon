49610ed9c403e7757616150aad0f044269f5284e
Elvedin <elvedinedo19@gmail.com>
2025-08-19 16:53:18 +0200

    Dodane default vrijednosti na pojedinim attributima.

--- src/main/java/com/example/dzanicprojekat/Entities/Termin.java
+++ src/main/java/com/example/dzanicprojekat/Entities/Termin.java
@@ -4,8 +4,14 @@ package com.example.dzanicprojekat.Entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Entity
@@ -21,16 +27,25 @@ public class Termin {
 
     @Column(nullable = false,updatable = false)
     private LocalDateTime createdAt;
-    private LocalDateTime start_time;
-    private LocalDateTime end_time;
-    private double price_full;
-    private double discount;
-    private double price_final;
-    private String client_name;
-    private String client_contact;
+    private LocalDate datum_termina;
+    private LocalTime pocetak_termina;
+    private double cijena;
+    @ColumnDefault("0.0")
+    private double popust;
+    @ColumnDefault("0")
     private boolean confirmed;
+    @ColumnDefault("0")
     private boolean canceled;
+    @ColumnDefault("'N/A'")
     private String cancellation_reason;
+
+    @ManyToMany()
+    @JoinTable(
+            name = "termin_usluga",
+            joinColumns = @JoinColumn(name = "termin_id"),
+            inverseJoinColumns = @JoinColumn(name = "usluga_id")
+    )
+    private List<Usluga> usluge;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
@@ -46,16 +61,12 @@ public class Termin {
     // termin_id:1 <----> usluga_id:1
     // termin_id:1 <----> usluga_id:2
     // termin_id:2 <----> usluga_id:3
-    @ManyToMany
-    @JoinTable(
-            name = "termin_usluga",
-            joinColumns = @JoinColumn(name = "termin_id"),
-            inverseJoinColumns = @JoinColumn(name = "usluga_id")
-    )
-    private List<Usluga> usluge;
+
 
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
+        this.pocetak_termina = LocalTime.MIDNIGHT;
+        this.datum_termina = LocalDate.now();
     }
 }
